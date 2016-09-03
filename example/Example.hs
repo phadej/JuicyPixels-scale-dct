e40e1f9f5c9ce02b8c36db177b3b17ce11f08d07
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-09-03 12:46:56 +0300

    Remove JuicyPixels-util dependency

--- example/Example.hs
+++ example/Example.hs
@@ -1,18 +1,26 @@
 module Main (main) where
 
-import Codec.Picture          (readImage, writePng)
-import Codec.Picture.RGBA8    (fromDynamicImage)
+import Codec.Picture          (convertRGBA8, readImage, writePng)
 import Codec.Picture.ScaleDCT (scale)
 import Data.Time              (diffUTCTime, getCurrentTime)
 
+processImage
+    :: String      -- ^ From
+    -> (Int, Int)  -- ^ Target size
+    -> String      -- ^ To
+    -> IO ()
+processImage src size dst = do
+    start <- getCurrentTime
+    Right dimg <- readImage src
+    let img = convertRGBA8 dimg
+    let ava = scale size img
+    writePng dst ava
+    end <- getCurrentTime
+    putStrLn $ dst ++ ": " ++ show (end `diffUTCTime` start)
+
 main :: IO ()
 main = do
-    start <- getCurrentTime
-    Right dimg <- readImage "phadej.png"
-    let img = fromDynamicImage dimg
-    let ava1 = scale (64, 64) img
-    let ava2 = scale (600, 600) img
-    writePng "phadej-small.png" ava1
-    writePng "phadej-large.png" ava2
-    end <- getCurrentTime
-    print $ end `diffUTCTime` start
+    processImage "cat.jpg" (96, 64) "cat-small.png"
+    processImage "cat.jpg" (769, 512) "cat-large.png"
+    processImage "phadej.png" (64, 64) "phadej-small.png"
+    processImage "phadej.png" (600, 600) "phadej-large.png"
